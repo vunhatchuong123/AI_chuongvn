11c9cb88c8086f8b7f728b6b05bf95cdf9317393
vunhatchuong123 <ronnyvu321@gmail.com>
2022-10-06 18:13:57 +0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AI_chuongvn
+
+Teacher [git](https://github.com/Pxtri2156/AI_Lectures)
+
+## Week 1: Water Jug
+
+Method: Breadth-first search with Queue. [Slide](https://docs.google.com/presentation/d/1OqDSN-AZ4_gWvJWqZekf7YdNXjg-9UGUbT9fNqwb360/edit#slide=id.p)
---
+++ Week1/Water_Jug.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+typedef pair<int, int> jugs;                              // define structure data for jugs
+void BFS(int a, int b, int target);                       // define BFS(Breadth-First Search) function
+void printresult(map<jugs, jugs> node, jugs currentNode); // define function print results
+
+int main()
+{
+
+  int a;
+  int b;
+  int target;
+  /// viet ham nhap a,b, target tu ban phim
+  // CODE HERE
+  cout << "Nhap a limit:";
+  cin >> a;
+  cout << "Nhap b limit: ";
+  cin >> b;
+  cout << "Nhap target: ";
+  cin >> target;
+  BFS(a, b, target);
+  return 0;
+}
+
+void printresult(map<jugs, jugs> node, jugs currentNode)
+{
+  if (currentNode.first == 0 && currentNode.second == 0)
+  {
+    cout << 0 << " " << 0 << endl;
+    return;
+  }
+  printresult(node, node[currentNode]);
+  cout << currentNode.first << " " << currentNode.second << endl;
+}
+
+void BFS(int a, int b, int target)
+{
+  map<jugs, int> isChecked; // kiem tra cac node da duyet: 1 da duyet, 0 chua duyet (default = 0)
+  bool isSolvable = false;  // ton tai ket qua
+  map<jugs, jugs> node;     // Graph
+  queue<jugs> q;            // Queue de duyet cay bang BFS
+
+  q.push(make_pair(0, 0)); // Bat dau duyet cay cho ket qua vao hanh doi
+  while (!q.empty())
+  {
+    jugs currentNode = q.front();    // lay phan tu dau tien
+    q.pop();                         // xoa phan tu dau tien khoi hang doi
+    if (isChecked[currentNode] == 1) // node da duyet thi pass
+      continue;
+    isChecked[{currentNode.first, currentNode.second}] = 1; // da duyet
+
+    if (currentNode.first == target || currentNode.second == target)
+    {
+      isSolvable = true;
+
+      printresult(node, currentNode);
+      // CODE HERE
+      return;
+    }
+    // ======= Operation =====
+    // ## Fill
+    // fill into jug 1
+    if (isChecked[{a, currentNode.second}] != 1)
+    {
+      q.push({a, currentNode.second});
+      node[{a, currentNode.second}] = currentNode; // Set father
+    }
+    // fill into jug 2
+    // CODE HERE
+
+    if (isChecked[{currentNode.first, b}] != 1)
+    {
+      q.push({currentNode.first, b});
+      node[{currentNode.first, b}] = currentNode;
+    }
+    // ## Transfer of water
+    int d = b - currentNode.second; // from jug 1 to jug 2
+    if (currentNode.first >= d)
+    {
+      int c = currentNode.first - d; // transfer a little
+      if (isChecked[{c, b}] != 1)
+      {
+        q.push({c, b});
+        node[{c, b}] = currentNode;
+      }
+    }
+    else
+    {
+      int c = currentNode.first + currentNode.second;
+      if (isChecked[{0, c}] != 1)
+      { /// transfer all
+        q.push({0, c});
+        node[{0, c}] = currentNode;
+      }
+    }
+    // from jug 2 to j1
+    // CODE HERE
+
+    if (isChecked[{currentNode.first, 0}] != 1)
+    {
+      q.push({currentNode.first, 0});
+      node[{currentNode.first, 0}] = currentNode;
+    }
+
+    if (isChecked[{0, currentNode.second}] != 1)
+    {
+      q.push({0, currentNode.second});
+      node[{0, currentNode.second}] = currentNode;
+    }
+  }
+  if (!isSolvable)
+    cout << "No solution";
+}
