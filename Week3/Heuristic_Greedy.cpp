e8d7893907a9e1f6a095b01291edf2ab5a0163c5
chuong vu <ronny@DESKTOP-PU9U7OG.localdomain>
2022-10-22 20:31:51 +0700

    Add Week3 Task find Staff

--- Week3/Heuristic_Greedy.cpp
+++ Week3/Heuristic_Greedy.cpp
@@ -10,7 +10,10 @@ int getMinTime(int, int[][100], int[], int);
 void showResults(int[], int);
 int findFirstIndex(int[], int);
 
+
+void showResults2(int[], int);
 void assignTaskOp2(int, int[][100], int[]);
+int getMinTime2(int, int[][100], int[], int);
 
 int main() {
   // b0: results[N]; Task [];
@@ -21,7 +24,8 @@ int main() {
   cin >> N;
   enterTable(N, table);
   showTable(N, table);
-  assignTask(N, table, results);
+  // assignTask(N, table, results);
+  assignTaskOp2(N, table, results);
   // b1: nhap bang cong viec = nhap mang 2 chieu => function
   // op1: nguoi tim viec
   // b2: Xet hang => for
@@ -31,10 +35,18 @@ int main() {
   // op2: viec tim nguoi
   //  .................
   // b3: xuat ket qua
-  showResults(results, N);
+  // showResults(results, N);
+  showResults2(results, N);
   return 0;
 }
 
+/*
+[0,0]5 6 4 7 2
+[1,0]5 2 4 5 1
+[2,0]4 5 4 6 3
+[3,0]5 5 3 4 2
+[4,0]3 3 5 2 5
+*/
 void enterTable(int N, int table[][100]) {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
@@ -89,13 +101,28 @@ int findFirstIndex(int task[], int N) {
   return -1;
 }
 
-void assignTaskOp2(int, int[][100], int[]) {
+void assignTaskOp2(int N, int table[][100], int results[]) {
   // CODE HERE
+  int task[N] = {0}; // danh sach task da lam (1) hay chua (0)
+  for (int i = 0; i < N; i++) {
+    int t = getMinTime2(i, table, task, N);
+    task[t] = 1;
+    results[i] = t;
+  }
 }
-/*
-5 6 4 7 2
-5 2 4 5 1
-4 5 4 6 3
-5 5 3 4 2
-3 3 5 2 5
-*/
+  
+void showResults2(int results[], int N) {
+  for (int i = 0; i < N; i++) {
+    cout << "Task " << i + 1 << ": " << results[i] + 1 << endl;
+  }
+}
+int getMinTime2(int column, int table[][100], int task[], int N) {
+  // find first index
+  int minIndex = findFirstIndex(task, N);
+  for (int j = 0; j < N; j++) {
+    if (table[j][column] < table[column][minIndex] && task[j] == 0) {
+      minIndex = j;
+    }
+  }
+  return minIndex;
+}
